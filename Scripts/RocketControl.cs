0c86360d686b3f95882cfb3018e8ae0ed6a73a80
Kyerstin O'Neal <kcrocker1@cnm.edu>
2021-04-18 21:27:44 -0600

    Initial commit

---
+++ Scripts/RocketControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketControl : MonoBehaviour
+{
+    public float turningForce;
+    
+    private Rigidbody rigidBody;
+
+    private bool engineOn;
+
+    public float engineForce;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get our mouse controls
+        float yaw = Input.GetAxis("Mouse X");
+        float pitch = Input.GetAxis("Mouse Y");
+
+        //Rotate the rocket using the mouse controls
+        rigidBody.AddRelativeTorque(
+            pitch * turningForce * Time.deltaTime, // Pitch
+            yaw * turningForce * Time.deltaTime, // Yaw
+            0f);                                 // Roll
+
+        // Turn on the rocket engine when W is pressed
+        if(Input.GetKeyDown(KeyCode.W))
+
+        {
+            engineOn = true;
+        }
+
+
+            // Turn off the rocket engine when W is released
+            if (Input.GetKeyUp(KeyCode.W))
+
+        {
+            engineOn = false;
+        }
+
+            //Quite when you hit escape
+            if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+            // If the engine is on
+            if(engineOn)
+        {
+            //Apply an engine force
+            rigidBody.AddRelativeForce(transform.forward * engineForce * Time.deltaTime);
+        }
+        
+    }
+}
---
+++ Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float degreesPerSecond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("The start method was run!");
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Time.deltaTime);
+        //Rotate the object by a certain speed on the Y axis  
+        transform.Rotate(0, degreesPerSecond * Time.deltaTime, 0);
+    }
+}
